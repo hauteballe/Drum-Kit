d09306ffdf498474a4c918d8c293442f557f364c
Irina Sinitsyna <hauteballe@gmail.com>
2020-12-08 19:28:10 +0300

    refactor: apply prettier in main.js

--- main.js
+++ main.js
@@ -13,5 +13,5 @@ function removeTransition(event) {
 }
 
 const keys = document.querySelectorAll(".key");
-keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
 window.addEventListener("keydown", playSound);
