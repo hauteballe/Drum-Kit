7f1d8ba8098f045a4ac33dd89be393e1b5c3c793
Irina Sinitsyna <hauteballe@gmail.com>
2020-12-09 17:54:31 +0300

    feat: implement clicking on the buttons with a computer mouse

--- main.js
+++ main.js
@@ -1,17 +1,48 @@
-function playSound(event) {
-  const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
-  const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
-  if (!audio) return;
-  audio.currentTime = 0;
-  audio.play();
-  key.classList.add("playing");
-}
+const app = () => {
+  function playSound(event) {
+    const audio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    key.classList.add("playback");
+  }
 
-function removeTransition(event) {
-  if (event.propertyName !== "transform") return;
-  event.target.classList.remove("playing");
-}
+  function removeBacklight(event) {
+    const key = document.querySelector(`.key[data-key="${event.keyCode}"]`);
+    if (!key) return;
+    key.classList.remove("playback");
+  }
 
-const keys = document.querySelectorAll(".key");
-keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
-window.addEventListener("keydown", playSound);
+  window.addEventListener("keydown", playSound);
+  window.addEventListener("keyup", removeBacklight);
+
+  function playOnClick(event) {
+    let elem = event.target;
+    if (elem.tagName !== "DIV") {
+      elem = elem.closest("div");
+    }
+    const audio = document.querySelector(
+      `audio[data-key="${elem.dataset.key}"]`
+    );
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+    elem.classList.add("playback");
+  }
+
+  const keys = document.querySelectorAll(".key");
+  keys.forEach((key) => key.addEventListener("click", playOnClick));
+
+  function removeBacklightMouseUp(event) {
+    let elem = event.target;
+    if (elem.tagName !== "DIV") {
+      elem = elem.closest("div");
+    }
+    if (!elem) return;
+    elem.classList.remove("playback");
+  }
+
+  window.addEventListener("transitionend", removeBacklightMouseUp);
+};
+app();
